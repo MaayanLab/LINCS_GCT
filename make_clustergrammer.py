6165a92e97ec48e19909a1d4708500dd45fbba30
cornhundred <doctorcorn2002@gmail.com>
2016-04-21 18:26:46 -0400

    adding python from broad-institute and Zichen-modified l1ktoolsrepo

--- make_clustergrammer.py
+++ make_clustergrammer.py
@@ -1,9 +1,5 @@
 import time
-# import StringIO
-
 start_time = time.time()
-
-# import network class from Network.py
 
 from clustergrammer import Network
 net = Network()
@@ -13,21 +9,11 @@ net.load_file('txt/rc_two_cats.txt')
 
 views = ['N_row_sum','N_row_var']
 
-# # filtering rows and cols by sum 
-# net.filter_sum('row', threshold=20)
-# net.filter_sum('col', threshold=30)
-  
-# # keep top rows based on sum 
-# net.filter_N_top('row', 10, 'sum')
+net.make_clust(dist_type='cos',views=views , dendro=True, sim_mat=True)
 
-net.make_clust(dist_type='cos',views=views , dendro=True,
-               sim_mat=True, filter_sim=0.1)
-
-# net.produce_view({'N_row_sum':10,'dist':'euclidean'})
-
-net.write_json_to_file('viz', 'json/mult_view.json', 'no-indent')
-net.write_json_to_file('sim_row', 'json/mult_view_sim_row.json', 'no-indent')
-net.write_json_to_file('sim_col', 'json/mult_view_sim_col.json', 'no-indent')
+net.write_json_to_file('viz', 'json/mult_view.json')
+net.write_json_to_file('sim_row', 'json/mult_view_sim_row.json')
+net.write_json_to_file('sim_col', 'json/mult_view_sim_col.json')
 
 elapsed_time = time.time() - start_time
 
