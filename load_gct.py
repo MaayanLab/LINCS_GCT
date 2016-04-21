5f7975b4d28f4c808fff5c521812ef10a69a88d7
cornhundred <doctorcorn2002@gmail.com>
2016-04-21 19:09:39 -0400

    l1ktools is able to load each file, working on transferring to dataframe

--- load_gct.py
+++ load_gct.py
@@ -1,41 +1,54 @@
-'''
-This script contains examples for reading .gctx files in Python.
-'''
-
-import cmap.io.gct as gct
-import cmap.io.plategrp as grp
-
-# give input file
-# path_to_gctx_file = '/xchip/cogs/l1ktools/data/modzs_n272x978.gctx'
-path_to_gctx_file = 'gcts/LDS-1207.gct'
-
-# read the full data file
-GCTObject = gct.GCT(path_to_gctx_file)
-GCTObject.read(verbose=False)
-
-print(GCTObject.matrix.shape)
-
-# get the available meta data headers for data columns and row
-cat_titles = {}
-# get the gene symbol meta data field from the row data
-cat_titles['row'] = GCTObject.get_rhd()
-# get the perturbagen description meta data field from the column data
-cat_titles['col'] = GCTObject.get_chd()
-
-for inst_rc in ['row', 'col']:
-
-  for inst_title in cat_titles[inst_rc]:
-
-    if inst_rc == 'row':
-      inst_cats = GCTObject.get_row_meta(inst_title)
-
-    elif inst_rc == 'col':
-      inst_cats = GCTObject.get_column_meta(inst_title)
   
+def main():
+  import glob
+  all_paths = glob.glob('gcts/*')
 
-    print('\nfound '+ str(len(inst_cats)) + ' categories for ' + inst_title)
+  for inst_filename in all_paths[:1]:
+    
+    print('\n'+inst_filename)
+    try:
+      load_file(inst_filename)
+    except:
+      print('failed')
+
+def load_file(filename):
+
+  import cmap.io.gct as gct
+  import cmap.io.plategrp as grp
+
+  GCTObject = gct.GCT(filename)
+  GCTObject.read(verbose=False)
+
+  print(GCTObject.matrix.shape)
+
+  # get the available meta data headers for data columns and row
+  cat_titles = {}
+  # get the gene symbol meta data field from the row data
+  cat_titles['row'] = GCTObject.get_rhd()
+  # get the perturbagen description meta data field from the column data
+  cat_titles['col'] = GCTObject.get_chd()
+
+  for inst_rc in ['row', 'col']:
+
+    for inst_title in cat_titles[inst_rc]:
+
+      if inst_rc == 'row':
+        inst_cats = GCTObject.get_row_meta(inst_title)
+
+      elif inst_rc == 'col':
+        inst_cats = GCTObject.get_column_meta(inst_title)
+    
+
+      # print('\nfound '+ str(len(inst_cats)) + ' categories for ' + inst_title)
+      # print(inst_cats)
+
+
+      ## ind is an index 
+
+      ## row id is the name of the row
+      ## col id is some id-number, I want to use smName (probably)
 
 
 
 
-
+main()
