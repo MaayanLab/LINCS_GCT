e5ccc1fd8cfeb30b4aea71f28de5ca216029e9cf
cornhundred <doctorcorn2002@gmail.com>
2016-04-22 14:47:04 -0400

    removing categories that are all the same or are unique to each data point

--- load_gct.py
+++ load_gct.py
@@ -72,7 +72,14 @@ def load_file(filename):
       elif inst_rc == 'col':
         inst_cats = GCTObject.get_column_meta(inst_title)
 
-      cat_info[inst_rc][inst_title] = inst_cats
+      num_data = len(inst_cats)
+
+      num_unique_cats = len(list(set(inst_cats)))
+
+      if num_unique_cats > 1 and num_unique_cats < num_data:
+
+        cat_info[inst_rc][inst_title] = inst_cats 
+
 
     # define metadata: names, categories with optional titles 
     meta_data[inst_rc] = []
@@ -88,18 +95,19 @@ def load_file(filename):
 
       all_titles = cat_titles[inst_rc]
 
-      # if inst_rc == 'row':
-      #   all_titles = ['clName','ppName']
-      # else:
-      #   all_titles = ['datapointUnit']
-
       for inst_title in all_titles:
 
-        # get individual category
-        inst_cat = inst_title+ ': '+ cat_info[inst_rc][inst_title][i]
+        if inst_title != 'ind' and inst_title !='id':
 
-        name_tuple = name_tuple + (inst_cat,)
 
+          if inst_title in cat_info[inst_rc]:
+            
+            # get individual category
+            inst_cat_name = inst_title+ ': '+ cat_info[inst_rc][inst_title][i]
+
+            name_tuple = name_tuple + (inst_cat_name,)
+
+      # write the entire tuple 
       meta_data[inst_rc].append( name_tuple )
 
   mat = GCTObject.matrix 
