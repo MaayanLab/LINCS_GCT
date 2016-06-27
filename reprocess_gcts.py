9bed6863923317e407471246940511830761b3eb
cornhundred <doctorcorn2002@gmail.com>
2016-06-27 17:13:52 -0400

    working on making visualizations from Anders vis-gcts

--- reprocess_gcts.py
+++ reprocess_gcts.py
@@ -6,6 +6,28 @@ def main():
   for inst_filename in all_paths:
 
     name = inst_filename.split('/')[1].split('.gct')[0]
+
     print(name)
 
+    try:
+      df = load_file(inst_filename)
+      print('works')
+
+    except:
+      print('did not work')
+
+    print('\n')
+
+def load_file(filename):
+  import pandas as pd
+  import cmap.io.gct as gct
+  import cmap.io.plategrp as grp
+
+  GCTObject = gct.GCT(filename)
+  GCTObject.read(verbose=False)
+
+  print(GCTObject.matrix.shape)
+
 main()
+
+
